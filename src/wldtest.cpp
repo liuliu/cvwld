1e13400c2a99594d70e28a2bacc7558eb8a57ef8
Liu Liu <liuliu.1987@gmail.com>
2010-01-21 19:24:53 -0500

    modified:   wldtest.cpp

--- src/wldtest.cpp
+++ src/wldtest.cpp
@@ -1,12 +1,16 @@
 #include "cvwld.h"
 #include "highgui.h"
 #include <assert.h>
+#include <stdio.h>
 
 int main(int argc, char** argv)
 {
 	assert(argc == 2);
 	IplImage* image = cvLoadImage(argv[1]);
+	double t = (double)cvGetTickCount();
 	CvMat* hr = cvExtractWLD(image, cvWLDParams(8, 6, 20));
+	t = (double)cvGetTickCount() - t;
+	printf("extract time = %gms\n", t / ((double)cvGetTickFrequency() * 1000.));
 	CvMat* result = cvCreateMat(100, 20 * 6 * 8, CV_8UC3);
 	cvNamedWindow("result", CV_WINDOW_AUTOSIZE);
 	for (int i = 0; i < 20 * 6 * 8; i++)
